f6f62495c8157780ae4b3cf3c439941b78feb932
Lucas de Farias <defarias.lucas@gmail.com>
2020-05-09 17:24:12 +0200

    :art: format code

--- src/use-cases/upload-document.js
+++ src/use-cases/upload-document.js
@@ -1,32 +1,35 @@
-const path = require('path');
+const path = require("path");
 
-module.exports = ({ fileService, converterService, ctx }) => async (message) => {
-    const { document } = message;
+module.exports = ({ fileService, converterService, ctx }) => async (
+  message
+) => {
+  const { document } = message;
 
-    const documentData = await fileService.getFileData(document.file_id);
-    const fileResponse = await fileService.downloadFile(documentData.file_path);
-    
-    const userDocumentsPath = path.resolve(__dirname, '../../storage/uploads', `${message.from.id}`);
-    const uploadedFileName = `${document.file_unique_id}${document.file_name}`;
-    const inputFilePath = await fileService.storeFile(
-        fileResponse.data, 
-        userDocumentsPath, 
-        uploadedFileName
-    );
+  const documentData = await fileService.getFileData(document.file_id);
+  const fileResponse = await fileService.downloadFile(documentData.file_path);
 
-    try {
-        const mobiBuffer = await converterService.toMobi({
-            fileName: document.file_name,
-            filePath: inputFilePath,
-        });
+  const userDocumentsPath = path.resolve(
+    __dirname,
+    "../../storage/uploads",
+    `${message.from.id}`
+  );
+  const uploadedFileName = `${document.file_unique_id}${document.file_name}`;
+  const inputFilePath = await fileService.storeFile(
+    fileResponse.data,
+    userDocumentsPath,
+    uploadedFileName
+  );
 
-        // TODO Send to kindle email
-        
+  try {
+    const mobiBuffer = await converterService.toMobi({
+      fileName: document.file_name,
+      filePath: inputFilePath,
+    });
 
-        // TODO Send notification
+    // TODO Send to kindle email
 
-    } catch (e) {
-        ctx.reply(`Ha habido un problema al procesar el documento: ${e.message}`);
-    }
-    
+    // TODO Send notification
+  } catch (e) {
+    ctx.reply(`Ha habido un problema al procesar el documento: ${e.message}`);
+  }
 };
